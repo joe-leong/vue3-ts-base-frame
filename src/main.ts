494dc1be15dcf6fbc40357cf4a9c4b911e7fd25f
梁国秋 <zdnf_lgq@163.com>
2023-03-29 18:50:31 +0800

    feat: 引入路由

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
+import { setupRouter } from "./router";
 
-createApp(App).mount("#app");
+const bootstrap = () => {
+  const app = createApp(App);
+
+  // 安装初始化路由
+  setupRouter(app);
+
+  app.mount("#app");
+};
+
+// 启动
+bootstrap();
