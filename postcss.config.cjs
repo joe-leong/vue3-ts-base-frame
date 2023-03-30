c2ad2642af86d52367ed280bc6769f52294b4369
梁国秋 <zdnf_lgq@163.com>
2023-03-30 15:32:16 +0800

    perf: vite plugins

---
+++ postcss.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      overrideBrowserslist: [
+        'Android 4.1',
+        'iOS 7.1',
+        'Chrome > 31',
+        'ff > 31',
+        'ie >= 8',
+        '> 1%'
+      ],
+      grid: true
+    },
+    'postcss-flexbugs-fixes': {}
+    // require('postcss-flexbugs-fixes')
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import removeConsole from 'vite-plugin-remove-console'
+import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    removeConsole(),
+    importToCDN({
+      modules: [
+        // {
+        //   name: 'vue',
+        //   var: 'vue',
+        //   path: 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.47/vue.global.prod.min.js'
+        // }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
@@ -12,5 +26,16 @@ export default defineConfig({
       utils: resolve(__dirname, './src/utils'),
       apis: resolve(__dirname, './src/apis')
     }
-  }
+  },
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  },
+  css: {}
 })
