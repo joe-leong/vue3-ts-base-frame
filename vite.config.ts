eff47a30a2e5072d31ba5660c4a725b500d0e783
梁国秋 <zdnf_lgq@163.com>
2023-03-30 23:41:58 +0800

    perf: build with cdn

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,68 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 import removeConsole from 'vite-plugin-remove-console'
 import { Plugin as importToCDN } from 'vite-plugin-cdn-import'
+import { visualizer } from 'rollup-plugin-visualizer'
+const {
+  npm_package_dependencies_vue: vueVersion,
+  npm_package_dependencies_vue_router: routerVersion,
+  npm_package_dependencies_pinia: piniaVersion,
+  npm_package_dependencies_dayjs: dayjsVersion
+} = process.env
+
+function standardVersion(
+  version: string,
+  dep: string,
+  fileName: string
+): string {
+  return `https://cdnjs.cloudflare.com/ajax/libs/${dep}/${version.replace(
+    '^',
+    ''
+  )}/${fileName}`
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    visualizer(),
     vue(),
     removeConsole(),
     importToCDN({
       modules: [
-        // {
-        //   name: 'vue',
-        //   var: 'vue',
-        //   path: 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.47/vue.global.prod.min.js'
-        // }
+        {
+          name: 'vue',
+          var: 'Vue',
+          path: standardVersion(
+            vueVersion,
+            'vue',
+            'vue.runtime.global.prod.min.js'
+          )
+        },
+        {
+          name: 'vue-router',
+          var: 'VueRouter',
+          path: standardVersion(
+            routerVersion,
+            'vue-router',
+            'vue-router.global.prod.min.js'
+          )
+        },
+        {
+          name: 'vue-demi',
+          var: 'VueDemi',
+          path: standardVersion('0.13.11', 'vue-demi', 'index.iife.js')
+        },
+        {
+          name: 'pinia',
+          var: 'Pinia',
+          path: standardVersion(piniaVersion, 'pinia', 'pinia.iife.prod.min.js')
+        },
+        {
+          name: 'dayjs',
+          var: 'dayjs',
+          path: standardVersion(dayjsVersion, 'dayjs', 'dayjs.min.js')
+        }
       ]
     })
   ],
